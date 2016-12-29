fad8025a863919f06fb7f631fd37478f044be57e
Brad <bsackerman3@gmail.com>
2016-12-28 20:57:30 -0800

    Create sensu.sh

---
+++ sensu.sh
@@ -0,0 +1,2 @@
+curl -XPOST http://sensuserver.example.com:4567/check/request -d '{"subscribers": ["coolhost001"], "check":"restart_httpd"}'
+#https://www.godaddy.com/garage/tech/automating-orchestration-tasks-sensu/
